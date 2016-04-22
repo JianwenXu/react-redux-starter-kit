a6ac64c8431f29eb9d1b1148ff1115d0f8b1418b
lisen <ls@ruijie>
2016-04-22 14:42:00 +0800

    fix: while compiling, details of fatal error could not be seen when stats is undefined.

--- build/webpack-compiler.js
+++ build/webpack-compiler.js
@@ -10,25 +10,27 @@ export default function webpackCompiler (webpackConfig, statsFormat = DEFAULT_ST
     const compiler = webpack(webpackConfig)
 
     compiler.run((err, stats) => {
-      const jsonStats = stats.toJson()
-
       debug('Webpack compile completed.')
-      debug(stats.toString(statsFormat))
 
       if (err) {
         debug('Webpack compiler encountered a fatal error.', err)
         return reject(err)
-      } else if (jsonStats.errors.length > 0) {
-        debug('Webpack compiler encountered errors.')
-        debug(jsonStats.errors.join('\n'))
-        return reject(new Error('Webpack compiler encountered errors'))
-      } else if (jsonStats.warnings.length > 0) {
-        debug('Webpack compiler encountered warnings.')
-        debug(jsonStats.warnings.join('\n'))
       } else {
-        debug('No errors or warnings encountered.')
+        const jsonStats = stats.toJson()
+        debug(stats.toString(statsFormat))
+
+        if (jsonStats.errors.length > 0) {
+          debug('Webpack compiler encountered errors.')
+          debug(jsonStats.errors.join('\n'))
+          return reject(new Error('Webpack compiler encountered errors'))
+        } else if (jsonStats.warnings.length > 0) {
+          debug('Webpack compiler encountered warnings.')
+          debug(jsonStats.warnings.join('\n'))
+        } else {
+          debug('No errors or warnings encountered.')
+        }
+        resolve(jsonStats)
       }
-      resolve(jsonStats)
     })
   })
 }
